fb205df5a7cd142cd9de5c7dfea658601c4d3611
Pavel Bogdanov <pasha.arkus@gmail.com>
2026-06-27 13:06:15 +0300

    перевёл на kts

---
+++ android/build.gradle.kts
@@ -0,0 +1,39 @@
+group = "org.ochamodev.simple_rsa3"
+version = "1.0"
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:9.2.1")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+plugins {
+    id("com.android.library")
+}
+
+android {
+    namespace = "org.ochamodev.simple_rsa3"
+
+    compileSdk = 36
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    defaultConfig {
+        minSdk = 24
+    }
+}
